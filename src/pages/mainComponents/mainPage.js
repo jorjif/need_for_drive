5f7edb203c08e1097f21c0a3bb318a455368d4fb
jorji <panenko01@mail.ru>
2021-07-03 20:36:59 +0300

    Сделал основную разметку сайта

--- src/pages/mainComponents/mainPage.js
+++ src/pages/mainComponents/mainPage.js
@@ -1,5 +1,41 @@
 import { NavBar } from "./nav.js";
+import { ReactComponent as Geo } from "../icons/geomark.svg";
+import "./mainPage.scss";
 function MainPage(props) {
-  return <NavBar />;
+  function orderChange(e) {
+    e.target.style.backgroundColor = `background: linear-gradient(
+      90deg,
+      hsl(148, 67%, 41%) 2.61%,
+      hsl(158, 76%, 32%) 112.6%
+    );`;
+  }
+  return (
+    <div className="page">
+      <NavBar />
+      <div className="main-page">
+        <header className="main-page_header">
+          <h1>Need for drive</h1>
+          <div className="main-page_geo">
+            <Geo />
+            <p>Ульяновск</p>
+          </div>
+        </header>
+        <main className="main-page_main">
+          <h1>Каршеринг</h1>
+          <h2>Need for drive</h2>
+          <p>Поминутная аренда авто твоего города</p>
+          <button className="main-page_main_order" onClick={(e) => orderChange(e)}>
+            Забронировать
+          </button>
+        </main>
+        <footer className="main-page_main_footer">
+          <p>© 2016-2019 «Need for drive»</p>
+          <p>
+            <a href="tel:+7495-234-2244">8 (495) 234-22-44</a>
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
 }
 export { MainPage };
