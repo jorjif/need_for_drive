25a879deb775962d9c83f862bc4648e460f0c178
jorji <panenko01@mail.ru>
2021-07-26 17:33:56 +0300

    v04 Добавил недостающие пункты в информации о заказе

--- src/pages/orderPageComponents/orderWindows/optionsPage.js
+++ src/pages/orderPageComponents/orderWindows/optionsPage.js
@@ -6,6 +6,10 @@ export default function OptionsPage(props) {
   const orderBoilerplate = {
     "Пункт выдачи": "Ульяновск, Нариманова\u00A042",
     Модель: "Hyundai,\u00A0i30N",
+    Цвет: "Голубой",
+    "Длительность аренды": "1д2ч",
+    Тариф: "На\u00A0сутки",
+    "Полный бак": "Да",
   };
   return (
     <div className="page_order_content">
